bab6aa35b47039f297f803dd3d9ba0586677364c
Samadhan <katkarsamadhan5@gmail.com>
2022-11-07 19:33:18 +0530

    new add method added

--- Test.java
+++ Test.java
@@ -1,5 +1,20 @@
 Public class Test{
+<<<<<<< Updated upstream
 public void Additon(){
 Sysrem.out.println("Addtion method")
 }
+=======
+public static void main (String[] aegs){
+int a=10;
+int b=20;
+int c=a+b;
+System.out.println(c);
 }
+
+>>>>>>> Stashed changes
+}
+
+public class demo{
+public void Add()
+{System.out.println("new add method");
+}}
